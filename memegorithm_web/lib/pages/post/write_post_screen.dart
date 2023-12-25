eb84baa554580c7797be884a9aa10cd960ded14a
seongwon980 <poiuy98749@yonsei.ac.kr>
2023-12-25 15:50:32 +0900

    Add: Fixed domain (memegorithm.ngrok.io) for convenience

--- memegorithm_web/lib/pages/post/write_post_screen.dart
+++ memegorithm_web/lib/pages/post/write_post_screen.dart
@@ -452,7 +452,7 @@ class _WritePostState extends State<WritePostScreen> {
 
   Future<String> sendPostRequest(String message) async {
     // String url = 'http://165.132.46.82:30527/'; // When using vessl
-    String url = 'https://b4ea8a541a39.ngrok.app/'; // When using ngrok
+    String url = 'https://memegorithm.ngrok.io/'; // When using ngrok
     Map<String, String> headers = {"Content-Type": "application/json"};
     String jsonBody = json.encode({'text': message});
 
